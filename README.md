e4c50c6c09ad16f3113e788ac970d480d5a313dc
Dulmini Guruge <agg362@usask.ca>
2023-11-28 11:08:45 -0600

    filter,map,sort

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 Run the below command to create routers in the project.
 
 >npm i react-router-dom@6
+
+
+Then add BrowerRouter tags around the App tag in index.js </br>
+Then add "import {Routes,Route,Link } from 'react-router-dom';" in app.js
+
+filter()
+
+- The filter method creates a copy of the array, filtered down to just the elements from the original array that pass the test. In order words, it will return a new list with just the elements that fulfil the condition.
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Desserts from './Desserts';
+import {Routes,Route,Link } from 'react-router-dom';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav classname="nav">
+        <Link to="#" className='nav-item'>Desserts</Link>
+        
+      </nav>
+      <Routes>
+        <Route path="/" element={<Desserts/>}/>
+        
+
+      </Routes>
     </div>
   );
 }
