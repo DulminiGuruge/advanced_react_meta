7963c59e645cc891349db9ebb9a9f2e5c56d5bf2
Dulmini Guruge <agg362@usask.ca>
2023-12-01 16:53:40 -0600

    Updated

--- src/Goals.js
+++ src/Goals.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 function GoalForm(props){
     const [formData, setFormData] = React.useState({goal:"",by:""});
@@ -14,6 +15,7 @@ function GoalForm(props){
         setFormData({goal:"",by:""});
        
     }
+
 
     return(
         <>
@@ -47,6 +49,53 @@ function ListOfGoals(props){
 
 }
 
+
+function GiftCard(){
+
+    const [giftCard, setGiftCard] = useState(
+        {
+            firstName: "Jennifer",
+            lastName: "Smith",
+            text: "Free dinner for 4 guests",
+            valid: true,
+            instructions: "To use your coupon, click the button below.",
+        }
+      );
+      function spendGiftCard() {
+        setGiftCard(prevState => {
+            return {
+              ...prevState,
+              text: "Your coupon has been used.",
+              valid: false,
+              instructions: "Please visit our restaurant to renew your gift card.",
+            }
+        });
+      }
+      return (
+        <div style={{padding: '40px'}}>
+          <h1>
+            Gift Card Page
+          </h1>
+          <h2>
+            Customer: {giftCard.firstName} {giftCard.lastName}
+          </h2>
+          <h3>
+            {giftCard.text}
+          </h3>
+          <p>
+            {giftCard.instructions}
+          </p>
+          {
+            giftCard.valid && (
+              <button onClick={spendGiftCard}>
+                Spend Gift Card
+              </button>
+            )
+          }
+        </div>
+      );
+}
+
 export default function Goals(){
     const [allGoals, updateAllGoals] = React.useState([]);
 
@@ -58,6 +107,7 @@ export default function Goals(){
         <div className="App">
             <GoalForm onAdd={addGoal}/>
             <ListOfGoals allGoals={allGoals}/>
+            <GiftCard/>
         </div>
     )
 
